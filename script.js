578075e0e287654b2bade8eccbd2d275fb76b5a4
Marco Pla <marco.pla@gmail.com>
2024-05-18 22:24:40 -0300

    active class added to the buttons

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+console.log(botoes);
 
 focoBt.addEventListener('click', () => {
   alterarContexto('foco');
+  focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
   alterarContexto('descanso-curto');
+  curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
   alterarContexto('descanso-longo');
+  longoBt.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+  botoes.forEach(function (elementoDaLista) {
+    elementoDaLista.classList.remove('active');
+  });
   html.setAttribute('data-contexto', contexto);
   banner.setAttribute('src', `/imagens/${contexto}.png`);
   switch (contexto) {
