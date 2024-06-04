195e3eb13462bd447968baaaf65c4a5a27eff6c7
Marco Pla <marco.pla@gmail.com>
2024-06-03 21:39:30 -0300

    adding the element to ul

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,7 @@
 const btAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const formTextArea = document.querySelector('.app__form-textarea');
-const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 const ulTarefas = document.querySelector('.app__section-task-list');
 
 function criarElementoHTML(tarefa) {
@@ -9,18 +9,18 @@ function criarElementoHTML(tarefa) {
   li.classList.add('app__section-task-list-item');
 
   const svg = document.createElement('svg');
-  svg.innerHTML = `<svg>
+  svg.innerHTML = `
     <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
         <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-    </svg>
-  </svg>`;
+    </svg>`;
 
   const paragrafo = document.createElement('p');
   paragrafo.classList.add('app__section-task-list-item-description');
   paragrafo.textContent = tarefa.descricao;
 
   const botao = document.createElement('button');
+  botao.classList.add('app_button-edit');
 
   const imagemBotao = document.createElement('img');
   imagemBotao.setAttribute('src', './imagens/edit.png');
@@ -44,9 +44,14 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     descricao: formTextArea.value,
   };
   tarefas.push(tarefa);
+  const elementoTarefa = criarElementoHTML(tarefa);
+  ulTarefas.append(elementoTarefa);
   localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  formTextArea.value = '';
+  formAdicionarTarefa.classList.add('hidden');
 });
 
 tarefas.forEach((tarefa) => {
   const elementoTarefa = criarElementoHTML(tarefa);
+  ulTarefas.append(elementoTarefa);
 });
