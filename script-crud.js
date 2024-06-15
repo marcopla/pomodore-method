ac3890c5baf2bd81fa249a80b044da637bce699b
Marco Pla <marco.pla@gmail.com>
2024-06-14 23:19:18 -0300

    add app__section-task-list-item-active class to just one  selected button

--- script-crud.js
+++ script-crud.js
@@ -49,6 +49,11 @@ function criarElementoHTML(tarefa) {
 
   li.onclick = () => {
     paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+    document
+      .querySelectorAll('.app__section-task-list-item-active')
+      .forEach((elemento) => {
+        elemento.classList.remove('app__section-task-list-item-active');
+      });
     li.classList.add('app__section-task-list-item-active');
   };
 
